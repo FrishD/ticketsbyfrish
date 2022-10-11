b8a623494a284685d59a3e9ea0dfce9cfd6f7209
Shay <97289930+FrishD@users.noreply.github.com>
2022-10-11 10:09:20 +0300

    Add files via upload

---
+++ commands/botinfos.js
@@ -0,0 +1,21 @@
+const {
+  SlashCommandBuilder
+} = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('botinfo')
+    .setDescription('Info Bot'),
+  async execute(interaction, client) {
+    const embed = new client.discord.MessageEmbed()
+      .setColor('ff9600')
+      .setAuthor('Bot Info', client.user.avatarURL())
+      .setDescription('Bot Info.\n\nMade With ❤️ By Frish 💥')
+      .setFooter(client.config.footerText, client.user.avatarURL())
+      .setTimestamp();
+
+    await interaction.reply({
+      embeds: [embed]
+    });
+  },
+};
